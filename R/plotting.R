58807ce1d786dd6fb8b3febfe0a3c3734e34f559
kostadinoff <drkostadinkostadinov@gmail.com>
2025-11-23 21:40:01 +0200

    modified:   R/plotting.R

--- R/plotting.R
+++ R/plotting.R
@@ -515,10 +515,4 @@ plot_proportion_comparisons <- function(results,
                                           subtitle = subtitle,
                                           x = xlab,
                                           y = ""
-                            ) +
-                            theme(
-                                          plot.title = element_text(face = "bold", size = 14),
-                                          axis.text.y = element_text(size = 10),
-                                          panel.grid.minor = element_blank()
                             )
-}
